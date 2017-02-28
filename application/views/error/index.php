a3fc95f6aac2d49f73ee35ddb35bce0e26e3e849
Gustavo Ramos <gustavoramos.93@hotmail.com>
2017-02-28 15:13:59 -0300

    Alterações e correções na organização do projeto

--- application/views/error/index.php
+++
@@ -1,7 +0,0 @@
-<?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>
-
-<div class="container">
-    <p>
-		Esta é a página de erro. Será mostrado quando uma página (= controller / method) não existir.
-	</p>
-</div>
--- application/views/home/index.php
+++
@@ -1,6 +0,0 @@
-<?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>
-
-<div class="container">
-    <h2>PHP MVC</h2>
-    <p>Essa é a página inicial do projeto.</p>
-</div>
